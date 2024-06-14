4c2e8a98a7dabcaf5a8a447c5a0283c91d33a5ec
Vanessa Wei <ywei84@emory.edu>
2024-06-14 13:31:12 +0800

    Adding images

--- pages/task.js
+++ pages/task.js
@@ -1,11 +1,36 @@
-import {initJsPsych} from '../jspsych/dist/jspsych';
-import jsPsychHtmlKeyboardResponse from '../jspsych/disk/plugin-html-keyboard-response';
-
 const jsPsych = initJsPsych();
+var timeline = [];
 
-const trial = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: 'Hello world!',
-}
+const heading = {
+    type: jsPsychSurveyHtmlForm,
+    html: `
+    <div style='display: flex; justify-content: space-between; margin: 6vh 10vw 0 10vw'>
+        <div style='text-align: center;'>
+            <img src='../img/blue.png', alt='Blue Circle', style="width: 35vw; max-width: 400px">
+        </div>
+        <div style="display: flex; flex-direction: column; align-items: center; justify-content: space-evenly; margin: 2vw">
+            <img src="../img/blue.png" alt="Blue Circle" style="width: 5vw;">
+            <img src="../img/blue.png" alt="Blue Circle" style="width: 5vw; ">
+            <img src="../img/blue.png" alt="Blue Circle" style="width: 5vw;">
+        </div>
+    </div>
+    `,
+    button_label: "SUBMIT", 
 
-jsPsych.run([trial]);
+};
+
+timeline.push(heading)
+
+// var blue_trial = {
+//     type: jsPsychHtmlKeyboardResponse,
+//     stimulus: 'Color Matching Task',
+// };
+
+// // var blue_trial = {
+// //     type: jsPsychImageKeyboardResponse,
+// //     stimulus: '../img/blue.png',
+// // };
+
+// timeline.push(blue_trial)
+
+jsPsych.run(timeline);
