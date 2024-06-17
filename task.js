90d5d2562e18f6be61bba9f7c4636e244b28793e
Vanessa Wei <ywei84@emory.edu>
2024-06-17 16:26:20 +0800

    Applied Delta E

--- task.js
+++ task.js
@@ -20,19 +20,54 @@ var test_stimuli = [
     { stimulus: "img/trail_6.png", rgb: 'rgb(67, 145, 162)'},
 ];
 
-// Calculate color similarity, use euclidean distance on rgb values
-function colorSimilarity(corAnswer, guess) {
-    const [r1, g1, b1] = corAnswer.match(/\d+/g).map(Number);
-    const [r2, g2, b2] = guess.match(/\d+/g).map(Number);
+// Changed from https://github.com/antimatter15/rgb-lab/blob/master/color.js
+function rgb2lab(rgb){
+    const [r1, g1, b1] = rgb.match(/\d+/g).map(Number)
 
-    const maxD = Math.sqrt(Math.pow(Math.max(Math.abs(0 - r1), Math.abs(255 - r1)), 2) + Math.pow(Math.max(Math.abs(0 - g1), Math.abs(255 - g1)), 2) + Math.pow(Math.max(Math.abs(0 - b1), Math.abs(255 - b1)), 2))
+    var r = r1 / 255,
+        g = g1 / 255,
+        b = b1 / 255,
+        x, y, z;
+  
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+  
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+  
+    x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
+    y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
+    z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
+  
+    return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+  }
+  
+  // calculate the perceptual distance between colors in CIELAB
+  // https://github.com/THEjoezack/ColorMine/blob/master/ColorMine/ColorSpaces/Comparisons/Cie94Comparison.cs
+  // Changed from https://github.com/antimatter15/rgb-lab/blob/master/color.js
+  function colorSimilarity(rgbA, rgbB){
+    labA = rgb2lab(rgbA)
+    labB = rgb2lab(rgbB)
 
-    const euDistance = Math.sqrt(Math.pow(r1 - r2, 2) + Math.pow(g1 - g2, 2) + Math.pow(b1 - b2, 2))
+    var deltaL = labA[0] - labB[0];
+    var deltaA = labA[1] - labB[1];
+    var deltaB = labA[2] - labB[2];
+    var c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+    var c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+    var deltaC = c1 - c2;
+    var deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+    var sc = 1.0 + 0.045 * c1;
+    var sh = 1.0 + 0.015 * c1;
+    var deltaLKlsl = deltaL / (1.0);
+    var deltaCkcsc = deltaC / (sc);
+    var deltaHkhsh = deltaH / (sh);
+    var i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
 
-    const percentageSim = (1 - (euDistance / maxD)) * 100
-
-    return percentageSim
-}
+    return i < 0 ? 0 : 100 - Math.sqrt(i);
+  }
 
 // rgb task: guess rgb values
 var rgbTask = {
